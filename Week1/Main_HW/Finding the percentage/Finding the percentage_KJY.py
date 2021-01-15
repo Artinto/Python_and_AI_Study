ece7599ea768a2bb845362c4bf54c4ca1dbc6a41
Embeddedroid <71992269+Embeddedroid@users.noreply.github.com>
2021-01-15 19:14:01 +0900

    Create  Finding the percentage_KJY.py

---
+++ Week1/Main_HW/Finding the percentage/Finding the percentage_KJY.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    s=0.00
+    for i in student_marks[query_name]:
+        s+=i
+    avg=s/3
+    print('%.2f' %avg)
