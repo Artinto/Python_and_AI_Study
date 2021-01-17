8e3ef4884234d856c925234e5503b47d77fb4319
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2021-01-18 05:05:29 +0900

    Create Arithmetic Operators_LGY.py

---
+++ Week1/Main_HW/Arithmetic Operators/Arithmetic Operators_LGY.py
@@ -0,0 +1,18 @@
+#Arithmetic Operators (산술연산자)
+#STDIN = 'standard input'
+# line_1 : a + b
+# line_2 : a - b
+# line_3 : a * b
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    
+    
+    
+    
