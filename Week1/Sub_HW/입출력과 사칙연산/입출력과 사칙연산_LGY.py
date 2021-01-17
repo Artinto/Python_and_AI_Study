5542ee474bf479b88c58ea6fdc7e39e52e3bbfbc
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2021-01-18 08:00:05 +0900

    Create 입출력과 사칙연산_LGY.py

---
+++ Week1/Sub_HW/입출력과 사칙연산/입출력과 사칙연산_LGY.py
@@ -0,0 +1,59 @@
+# 1번
+print("Hello World!")
+
+# 2번
+for _ in range(2):
+    print("강한친구 대한육군")
+	
+# 3번
+print('\\    /\\\n )  ( \')\n(  /  )\n \\(__)|')
+
+# 4번
+print('|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|')
+
+# 5번
+a, b = map(int, input().split(' '))
+print(a + b)
+
+# 6번
+a, b = map(int, input().split(' '))
+print(a - b)
+
+# 7번
+a, b = map(int, input().split(' '))
+print(a * b)
+
+# 8번
+a, b = map(int, input().split(' '))
+print(a / b)
+
+# 9번
+a, b = map(int, input().split(' '))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+# 10번
+a, b, c = map(int, input().split(' '))
+print(((a + b)%c))
+print(((a%c) + (b%c))%c)
+print(((a * b)%c))
+print(((a%c) * (b%c))%c)
+
+# 10번
+a = int(input())
+b = input()
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
+
+
+
+
+
+
+
+
