a07f44217eb03dc5e6f7dff314df06408d54d052
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2021-01-18 16:28:58 +0900

    Update Print Function_KAY.py

--- Week1/Main_HW/Print Function/Print Function_KAY.py
+++ Week1/Main_HW/Print Function/Print Function_KAY.py
@@ -2,12 +2,5 @@ def funtion():
     n=int(input())
     num=0
     for i in range(1, n+1):
-        if i/10<1:
-            num=num+i*10**(n-i)
-        elif i/10>=1 and i/100<1:
-           num=num*10+i*10**(n-i)    
-        else:
-            num=num*100+i*10**(n-i)
+        print(i)
        
-    return num   
-print(funtion())   
