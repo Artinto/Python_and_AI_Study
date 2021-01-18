3e0f2730c56b13ed01cf14b62e187607a55fe7a0
dldnxks12 <59076451+dldnxks12@users.noreply.github.com>
2021-01-18 22:53:37 +0900

    Create List Comprehensions_LJS.py

---
+++ Week2/Main_HW/List Comprehensions/List Comprehensions_LJS.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    
+    sol = [ [a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if (a+b+c) != n]
+    
+    print(sol)
