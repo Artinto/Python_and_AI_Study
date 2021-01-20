f0d3f11d9fd7a0586c321db01ba743ed9e5ecb6d
Embeddedroid <71992269+Embeddedroid@users.noreply.github.com>
2021-01-20 23:46:49 +0900

    Update Nested Lists_KJY.py

--- Week2/Main_HW/Nested Lists/Nested Lists_KJY.py
+++ Week2/Main_HW/Nested Lists/Nested Lists_KJY.py
@@ -7,17 +7,23 @@ if __name__ == '__main__':
     name=[]
     score=[]
     
+    # 정렬을 위해 name과 score 위치 변경
     for i in range(len(s)):
         tmp=s[i][0]
         s[i][0]=s[i][1]
         s[i][1]=tmp
+    
+    # 오름차순 정렬 하면 첫번째 요소의 점수가 최솟값
     s.sort()
     mini=s[0][0]
+    
     while True:
         if s[0][0]==mini:
             s.remove(s[0])
         else:
             break
+    
+    # 새로운 리스트의 최솟값
     mini=s[0][0]
     lst=[]
     for i in range(len(s)):
