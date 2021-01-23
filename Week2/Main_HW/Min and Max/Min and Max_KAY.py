8e23d2652f102e963f3f79be2a701b8a372b39b2
a-young-kim <76930260+a-young-kim@users.noreply.github.com>
2021-01-23 19:13:07 +0900

    Create Min and Max_KAY.py

---
+++ Week2/Main_HW/Min and Max/Min and Max_KAY.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+n, m=input().split()
+arr1=np.array([], dtype=np.int8)
+
+for _ in range(int(n)):
+    a=np.array(list(map(int, input().split())))
+    arr1=np.concatenate((arr1, a))
+arr1=arr1.reshape(int(n), int(m))
+arr1=np.min(arr1, axis=1)    
+print(np.max(arr1))
