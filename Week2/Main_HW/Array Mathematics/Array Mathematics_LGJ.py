edf22dd156cddbba3c2295dfa2c15514386b7fd9
gijinLEE <44715550+gijinLEE@users.noreply.github.com>
2021-01-24 02:44:55 +0900

    Update Array Mathematics_LGJ.py

--- Week2/Main_HW/Array Mathematics/Array Mathematics_LGJ.py
+++ Week2/Main_HW/Array Mathematics/Array Mathematics_LGJ.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-N=[]
-M=[]
+A=[]
+B=[]
 
 n,m=map(int,input().split())
 
 for i in range(0,n):
-    N.append(list(map(int,input().split())))
+    A.append(list(map(int,input().split())))
 
 for i in range(0,n):
-    M.append(list(map(int,input().split())))
+    B.append(list(map(int,input().split())))
 
-narr = np.array(N, int)
-marr = np.array(M, int)
+Aarr = np.array(A, int)
+Barr = np.array(B, int)
 
-print(narr + marr)
-print(narr - marr)
-print(narr * marr)
-print(np.floor_divide(narr, marr))
-print(narr % marr)
-print(narr ** marr)
+print(Aarr + Barr)
+print(Aarr - Barr)
+print(Aarr * Barr)
+print(np.floor_divide(Aarr, Barr))
+print(Aarr % Barr)
+print(Aarr ** Barr)
