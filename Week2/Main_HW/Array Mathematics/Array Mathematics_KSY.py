ecbb3769195d2e212581207ed37c8897eb489384
atgs305 <77234878+atgs6591@users.noreply.github.com>
2021-01-24 22:02:10 +0900

    Create Array Mathematics_KSY.py

---
+++ Week2/Main_HW/Array Mathematics/Array Mathematics_KSY.py
@@ -0,0 +1,14 @@
+import numpy
+
+N,M=map(int,input().split())
+
+A=numpy.array([list(map(int,input().split())) for n in range(N)])
+B=numpy.array([list(map(int,input().split())) for n in range(N)])
+
+print(numpy.add(A,B))
+print(numpy.subtract(A,B))
+print(numpy.multiply(A,B))
+m=A/B
+print(numpy.int_(m))
+print(numpy.mod(A,B))
+print(numpy.power(A,B))
