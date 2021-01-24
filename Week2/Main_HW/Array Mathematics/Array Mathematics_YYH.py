707454ed5e14418cd8aa74a599a28eb534e861bb
qkrqhdud <77331618+qkrqhdud@users.noreply.github.com>
2021-01-24 20:04:38 +0900

    Create Array Mathematics_YYH.py

---
+++ Week2/Main_HW/Array Mathematics/Array Mathematics_YYH.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+N,M=map(int,input().split())
+
+
+a = np.array(list(input().split() for _ in range(N)), dtype=int)
+b = np.array(list(input().split() for _ in range(N)), dtype=int)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
+
