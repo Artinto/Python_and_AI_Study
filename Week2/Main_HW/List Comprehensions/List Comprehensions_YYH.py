865e4a765289fc35b5708176733a1faafab62e5d
qkrqhdud <77331618+qkrqhdud@users.noreply.github.com>
2021-01-24 18:32:41 +0900

    Create List Comprehensions_YYH.py

---
+++ Week2/Main_HW/List Comprehensions/List Comprehensions_YYH.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+
+    arr=[]
+    for a in range(0,x+1):
+        for b in range(0,y+1):
+            for c in range(0,z+1):
+                if a+b+c!=n:
+                    arr.append([a,b,c])
+    print(arr)
