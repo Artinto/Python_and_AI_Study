dd0eb089d2c343bea77623d5f308c9817e1cce6b
gijinLEE <44715550+gijinLEE@users.noreply.github.com>
2021-02-14 21:59:53 +0900

    Update auto_gradient_LGJ.py

--- Week5/03_auto_gradient/auto_gradient_LGJ.py
+++ Week5/03_auto_gradient/auto_gradient_LGJ.py
@@ -3,7 +3,7 @@ import pdb  # pdb(파이썬 디버거) 호출
 
 x_data = [1.0, 2.0, 3.0]  # x데이터 입력
 y_data = [2.0, 4.0, 6.0]  # y데이터 입력
-w = torch.tensor([1.0], requires_grad=True) # 값이 1.0인 1x1의 2차원 텐서 w를 선언하고, 이 텐서에 기울기를 저장하겠다
+w = torch.tensor([1.0], requires_grad=True) # 값이 1.0인 벡터 텐서 w를 선언하고, 이 w라는 텐서에 자동미분을 하여 그 기울기(미분값)를 w.grad에 저장함
 
 # our model forward pass
 def forward(x): # 포워드 함수 선언(x*w)
@@ -14,14 +14,14 @@ def loss(y_pred, y_val):  # 오차 함수 선언
     return (y_pred - y_val) ** 2
 
 # Before training
-print("Prediction (before training)",  4, forward(4).item())  # 기계 학습 전 대입. --> 무의미한 값이 나와야함
+print("Prediction (before training)",  4, forward(4).item())  # 기계 학습 전 4를 대입했을 때의 forward 값: 4 x 1.0 = 4.0
 
 # Training loop
 for epoch in range(10): # 10번 경사 하강 
     for x_val, y_val in zip(x_data, y_data):  # x_val, y_val에 각각 x_data, y_data의 쌍들을 집어넣음
         y_pred = forward(x_val) # 1) Forward pass
         l = loss(y_pred, y_val) # 2) Compute loss
-        l.backward() # 3) Back propagation to update weights, loss함수를 이용하여 기울기 계산
+        l.backward() # 3) Back propagation to update weights, loss함수를 이용하여 기울기 계산 --> 그 값이 w.grad에 저장됌
         print("\tgrad: ", x_val, y_val, w.grad.item())  # x_val, y_val, 기울기를 출력
         w.data = w.data - 0.01 * w.grad.item()  # w값에 따른 w-cost함수의 기울기가 음수면 오른쪽, 양수면 왼쪽으로 가도록 이동 --> 경사를 따라 내려가게됌 (학습률 0.01)
 
