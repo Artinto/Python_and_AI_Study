91836c771a9421078e398cc4fe8b0da5c111224a
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-02-22 01:51:51 +0900

    Create 06_logistic_regression_KYS.py

---
+++ Week6/06_logistic_regression/06_logistic_regression_KYS.py
@@ -0,0 +1,68 @@
+from torch import tensor        # tensor : 계산 속도를 빠르게 하기 위해 GPU를 사용할 수 있게 만들어줌
+from torch import nn            # 신경망을 생성할 수 있는 패키지
+from torch import sigmoid       # 로지스틱 함수인 sigmoid 사용 
+                                # linear model의 결과값을 sigmoid의 입력으로 넣어주면 0-1 사이의 값을 얻음
+import torch.nn.functional as F # nn == class / functional == function(객체 생성필요 X)
+import torch.optim as optim     # 최적화 알고리즘 정의
+
+# Training data and ground truth
+x_data = tensor([[1.0], [2.0], [3.0], [4.0]]) # input tensor
+y_data = tensor([[0.], [0.], [1.], [1.]])     # output tensor as binary
+
+
+class Model(nn.Module): # nn.Module을 상속받는  Model class 생성
+    def __init__(self): # initial
+        """
+        In the constructor we instantiate nn.Linear module
+        """
+        super(Model, self).__init__()  # 부모 클래스 생성자 호출
+        self.linear = nn.Linear(1, 1)  # One in and one out
+        # nn.Linear : 파이토치에 구현되어 있는 선형 회귀 모델
+        # parameter : first > 입력 차원 second > 출력 차원
+
+    def forward(self, x): # 선형 모델 계산
+        """
+        In the forward function we accept a Variable of input data and we must return
+        a Variable of output data.
+        """
+        y_pred = sigmoid(self.linear(x)) # sigmoid 의 결과값
+        return y_pred
+
+
+# our model
+model = Model() # Model 객체 생성
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+
+# 이진 분류에 특화된 loss 함수
+# reduction='mean' == 결과값의 평균
+criterion = nn.BCELoss(reduction='mean')
+
+# optimizer setting
+# 경사 하강법인 SGD를 사용하고 learning rate는 0.01로 설정
+# model.parameters()는 w(weight)와 b(bias)를 전달
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Training loop
+for epoch in range(1000): # == for(i = 0; i < 1000; i++)
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x_data) # 예측값 저장
+
+    # Compute and print loss
+    # 예측값(y_pred)과 결과값(y_data)의 loss를 구함
+    loss = criterion(y_pred, y_data)
+    print(f'Epoch {epoch + 1}/1000 | Loss: {loss.item():.4f}')
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad() # backword 하기 전 gradient를 초기화 (cuz, gradient가 덮여쓰여지지 않고 누적되기 때문)
+    loss.backward()       # loss에 대한 backword 진행
+    optimizer.step()      # parameter 초기화
+
+# After training
+print(f'\nLet\'s predict the hours need to score above 50%\n{"=" * 50}')
+hour_var = model(tensor([[1.0]])) # 학습 후 1.0을 입력 해 결과값 도출
+print(f'Prediction after 1 hour of training: {hour_var.item():.4f} | Above 50%: {hour_var.item() > 0.5}')
+hour_var = model(tensor([[7.0]])) # 학습 후 7.0을 입력 해 결과값 도출
+print(f'Prediction after 7 hours of training: {hour_var.item():.4f} | Above 50%: { hour_var.item() > 0.5}')
