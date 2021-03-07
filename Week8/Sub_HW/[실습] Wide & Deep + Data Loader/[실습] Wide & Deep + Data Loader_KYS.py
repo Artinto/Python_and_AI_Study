fd9eb27ed25562afcaf85d1d5796b232a047808b
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-07 22:10:45 +0900

    Create [실습]Wide & Deep + Data Loader_KYS.py

--- Week8/Sub_HW/[실습] Wide & Deep + Data Loader/[실습] Wide & Deep + Data Loader_KYS.py
+++ Week8/Sub_HW/[실습] Wide & Deep + Data Loader/[실습] Wide & Deep + Data Loader_KYS.py
@@ -29,10 +29,16 @@ y_test = torch.tensor(dataset[660:,8:9], dtype=torch.float32, device=device)
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.linear = nn.Linear(8, 1)
+        self.linear1 = nn.Linear(8, 6)
+        self.linear2 = nn.Linear(6, 4)
+        self.linear3 = nn.Linear(4, 1)
+
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        y_pred = sigmoid(self.linear(x))
+        out1 = self.sigmoid(self.linear1(x))
+        out2 = self.sigmoid(self.linear2(out1))   # out1의 결과를 out2의 input으로
+        y_pred = self.sigmoid(self.linear3(out2)) # out2의 결과를 out3의 input으로
         return y_pred
 
 print("========== Train Start ==========")
@@ -44,7 +50,7 @@ while lr >= 1e-4:
     optimizer = optim.SGD(model.parameters(), lr=lr)
     print("Learning Rate : ", lr)
     cnt = 0
-    for epoch in range(5001):
+    for epoch in range(4001):
         for x, y in loader:
             y_pred = model(x)
             loss = criterion(y_pred, y)
@@ -60,7 +66,7 @@ while lr >= 1e-4:
             optimizer.step()
         
         if epoch % 1000 == 0:
-            print("Accuracy : ", cnt / len(y_train) * 100)
+            print(f'Loss : {loss.item():.8f} | Accuracy : {cnt / len(y_train) * 100:.8f}')
             cnt = 0   
 
     with torch.no_grad():
@@ -71,3 +77,40 @@ while lr >= 1e-4:
         print("Test Accuracy : ", cnt / len(y_test) * 100)
 
     lr /= 10
+
+# ========== Train Start ==========
+# Learning Rate :  1
+# Loss : 0.69554746 | Accuracy : 65.75757576
+# Loss : 0.32509372 | Accuracy : 77.42424242
+# Loss : 0.43581358 | Accuracy : 80.60606061
+# Loss : 0.40938622 | Accuracy : 83.03030303
+# Loss : 0.23617199 | Accuracy : 84.84848485
+# Test Accuracy :  64.64646464646465
+# Learning Rate :  0.1
+# Loss : 0.68282759 | Accuracy : 65.75757576
+# Loss : 0.56766868 | Accuracy : 76.21212121
+# Loss : 0.33969310 | Accuracy : 76.81818182
+# Loss : 0.61289310 | Accuracy : 78.33333333
+# Loss : 0.53455538 | Accuracy : 78.78787879
+# Test Accuracy :  81.81818181818183
+# Learning Rate :  0.01
+# Loss : 0.63373005 | Accuracy : 65.75757576
+# Loss : 0.54773200 | Accuracy : 65.75757576
+# Loss : 0.64490074 | Accuracy : 65.75757576
+# Loss : 0.61195731 | Accuracy : 65.75757576
+# Loss : 0.61676788 | Accuracy : 65.75757576
+# Test Accuracy :  62.62626262626263
+# Learning Rate :  0.001
+# Loss : 0.64702386 | Accuracy : 65.75757576
+# Loss : 0.59672606 | Accuracy : 65.75757576
+# Loss : 0.62615573 | Accuracy : 65.75757576
+# Loss : 0.65563953 | Accuracy : 65.75757576
+# Loss : 0.65552121 | Accuracy : 65.75757576
+# Test Accuracy :  62.62626262626263
+# Learning Rate :  0.0001
+# Loss : 0.73249143 | Accuracy : 34.24242424
+# Loss : 0.69618034 | Accuracy : 34.24242424
+# Loss : 0.66721696 | Accuracy : 65.75757576
+# Loss : 0.64029694 | Accuracy : 65.75757576
+# Loss : 0.66393059 | Accuracy : 65.75757576
+# Test Accuracy :  62.62626262626263
