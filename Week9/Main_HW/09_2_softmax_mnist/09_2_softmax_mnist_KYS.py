d7faf4a626b0387c35b31ab39126a5d87c5e8fc4
kysu5095 <53482536+kysu5095@users.noreply.github.com>
2021-03-15 04:24:28 +0900

    Create 09_2_softmax_mnist_KYS.py

--- Week9/Main_HW/09_2_softmax_mnist/09_2_softmax_mnist_KYS.py
+++ Week9/Main_HW/09_2_softmax_mnist/09_2_softmax_mnist_KYS.py
@@ -1,21 +1,24 @@
 # https://github.com/pytorch/examples/blob/master/mnist/main.py
-from __future__ import print_function
-from torch import nn, optim, cuda
-from torch.utils import data
+from __future__ import print_function # __future__ : python3에서 python2 문법 사용 가능
+from torch import nn, optim, cuda 
+from torch.utils import data # for using DataLoader
+# torchvision : 이미지 변환을 위해
+# datasets : for using MNIST
+# transforms : 다양한 이미지 변환 제공 (현재 과제에서는 numpy -> tensor)
 from torchvision import datasets, transforms
 import torch.nn.functional as F
 import time
 
 # Training settings
-batch_size = 64
-device = 'cuda' if cuda.is_available() else 'cpu'
+batch_size = 64 # batch size(64) 지정
+device = 'cuda' if cuda.is_available() else 'cpu' # cuda 사용
 print(f'Training MNIST Model on {device}\n{"=" * 44}')
 
 # MNIST Dataset
-train_dataset = datasets.MNIST(root='./mnist_data/',
-                               train=True,
-                               transform=transforms.ToTensor(),
-                               download=True)
+train_dataset = datasets.MNIST(root='./mnist_data/', # data path
+                               train=True, # train data로 사용할지 안할지
+                               transform=transforms.ToTensor(), # 데이터 형식 지정
+                               download=True) # MNIST 없으면 download
 
 test_dataset = datasets.MNIST(root='./mnist_data/',
                               train=False,
@@ -24,7 +27,7 @@ test_dataset = datasets.MNIST(root='./mnist_data/',
 # Data Loader (Input Pipeline)
 train_loader = data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
-                                           shuffle=True)
+                                           shuffle=True) # data shuffle
 
 test_loader = data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
@@ -39,7 +42,7 @@ class Net(nn.Module):
         self.l2 = nn.Linear(520, 320)
         self.l3 = nn.Linear(320, 240)
         self.l4 = nn.Linear(240, 120)
-        self.l5 = nn.Linear(120, 10)
+        self.l5 = nn.Linear(120, 10)  # **10차원의 output**
 
     def forward(self, x):
         x = x.view(-1, 784)  # Flatten the data (n, 1, 28, 28)-> (n, 784)
@@ -52,19 +55,20 @@ class Net(nn.Module):
 
 model = Net()
 model.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+criterion = nn.CrossEntropyLoss() # CrossEntropyLoss 사용
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5) # optimizer로 SGD사용, learning rate = 0.01
+                                                                 # momentum을 사용해서 local minimum을 탈출 가능
 
 
 def train(epoch):
-    model.train()
+    model.train() # (Trian mode) 학습할 땐 무작위로 노드를 선택하여 선별적으로 노드를 활용함
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+        optimizer.zero_grad() # 기울기 0으로 초기화
+        output = model(data)  # 학습
+        loss = criterion(output, target) # get loss
+        loss.backward()  # 역전파
+        optimizer.step() # 매개변수 갱신
         if batch_idx % 10 == 0:
             print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -72,7 +76,7 @@ def train(epoch):
 
 
 def test():
-    model.eval()
+    model.eval() # (Evalutaion mode) 평가하는 과정에서는 모든 노드를 사용하겠다는 의미
     test_loss = 0
     correct = 0
     for data, target in test_loader:
@@ -81,8 +85,8 @@ def test():
         # sum up batch loss
         test_loss += criterion(output, target).item()
         # get the index of the max
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        pred = output.data.max(1, keepdim=True)[1] # One hot 벡터에서 값이 1인 index 가져오기
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum() # target과 pred를 비교하여 성공률 구하기
 
     test_loss /= len(test_loader.dataset)
     print(f'===========================\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} '
@@ -90,15 +94,15 @@ def test():
 
 
 if __name__ == '__main__':
-    since = time.time()
+    since = time.time() # 현재 시간
     for epoch in range(1, 10):
-        epoch_start = time.time()
-        train(epoch)
-        m, s = divmod(time.time() - epoch_start, 60)
+        epoch_start = time.time() # 시작 시간
+        train(epoch) # start train
+        m, s = divmod(time.time() - epoch_start, 60) # check train time
         print(f'Training time: {m:.0f}m {s:.0f}s')
-        test()
-        m, s = divmod(time.time() - epoch_start, 60)
+        test() # start test
+        m, s = divmod(time.time() - epoch_start, 60) # check test time
         print(f'Testing time: {m:.0f}m {s:.0f}s')
 
-    m, s = divmod(time.time() - since, 60)
+    m, s = divmod(time.time() - since, 60) # check total time
     print(f'Total Time: {m:.0f}m {s:.0f}s\nModel was trained on {device}!')
