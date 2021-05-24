7fc86b74e1f8e79436c5502abc3f063ca62b97d4
qkrqhdud <77331618+qkrqhdud@users.noreply.github.com>
2021-05-24 20:45:50 +0900

    Update Main_HW_YYH.py

--- Week16/MainHW/Main_HW_YYH.py
+++ Week16/MainHW/Main_HW_YYH.py
@@ -65,7 +65,7 @@ model.compile(loss='mean_squared_error', optimizer='adam')  #mean_squared_error 
                                                             #Adam( Adaptive Moment Estimation)은  각 파라미터마다 다른 크기의 업데이트를 적용하는 방법이다.            
 early_stop = EarlyStopping(monitor='loss', patience=3,verbose=1) #loss가 3번까지 좋아지지않으면 멈춤
 
-history = model.fit(x_train, y_train, 
+model.fit(x_train, y_train, 
                     epochs=200, 
                     batch_size=8,
                     callbacks=[early_stop])  #학습 시작
@@ -79,7 +79,7 @@ plt.legend()
 plt.show()
 
 #참고 https://teddylee777.github.io/tensorflow/LSTM%EC%9C%BC%EB%A1%9C-%EC%98%88%EC%B8%A1%ED%95%B4%EB%B3%B4%EB%8A%94-%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90-%EC%A3%BC%EA%B0%80
-#
+#https://user-images.githubusercontent.com/77331618/119235798-2c10e200-bb6f-11eb-83f2-761952ad3765.png
 
 
 
